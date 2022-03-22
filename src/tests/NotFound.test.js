1f7600a0527de5d42bd65dcf8be030f13a97f624
Jacqueline Silva <jacque.sx@hotmail.com>
2022-03-22 19:52:06 -0300

    Requisito 04 - Teste o componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Teste o componente NotFound', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const notFoundH2 = screen.getByRole('heading',
+      { level: 2,
+        name: /page requested not found/i,
+      });
+
+    expect(notFoundH2).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i });
+
+    expect(image).toHaveAttribute('src', url);
+    expect(image).toHaveAttribute('alt');
+    // class="not-found-image"
+  });
+});
